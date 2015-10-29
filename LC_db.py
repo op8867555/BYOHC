f8faf0be0fb0897a927aafac3312f830ca17d8e5
op8867555 <op8867555@gmail.com>
2015-10-29 13:44:59 +0800

    新增 eval 次數

--- LC_db.py
+++ LC_db.py
@@ -34,41 +34,43 @@ def subst(expr, name, val):
         return expr
 
 
-count = [0]
+def eval(expr, weak=False, debug=False, _state=None):
+    if not _state:
+        _state = {'count': 0, 'subst': 0}
 
-
-def _eval(expr, weak=False):
-    count[0] += 1
-    return eval(expr, weak)
-
-
-def eval(expr, weak=False):
-    typ = expr[0]
-    if typ == Var:
-        return expr
-    elif typ == Lam:
-        if weak:
+    def _eval(expr, weak=False):
+        _state['count'] += 1
+        typ = expr[0]
+        if typ == Var:
             return expr
-        else:
-            _lam, b = expr
-            return [_lam, _eval(b, weak)]
-    elif typ == App:
-        _app, f, v = expr
-        f_ = _eval(f, weak=True)
-        if f_[0] == Lam:
-            _lam, b = f_
-            return shift(_eval(subst(b, 0, shift(v, 1)), weak), -1)
-        elif f_[0] == Prim and f_[1] == Fun:
-            v_ = eval(v)
-            return f_[2](v_)
-        else:
+        elif typ == Lam:
             if weak:
                 return expr
             else:
-                return [_app, _eval(f), _eval(v)]
-    else:
-        return expr
+                _lam, b = expr
+                return [_lam, _eval(b, weak)]
+        elif typ == App:
+            _app, f, v = expr
+            f_ = _eval(f, weak=True)
+            if f_[0] == Lam:
+                _lam, b = f_
+                _state['subst'] += 1
+                return shift(_eval(subst(b, 0, shift(v, 1)), weak), -1)
+            elif f_[0] == Prim and f_[1] == Fun:
+                v_ = eval(v)
+                return f_[2](v_)
+            else:
+                if weak:
+                    return expr
+                else:
+                    return [_app, _eval(f), _eval(v)]
+        else:
+            return expr
+    r = _eval(expr, weak=weak)
+    if debug:
+        print(_state)
+    return r
 
 
-def eval_(expr):
-    return _eval(to_de_bruijn(expr))
+def eval_(expr, debug=False):
+    return eval(to_de_bruijn(expr), debug=debug)
